d26feccf00987961f69a63e08f4a3c97262674b3
gappc <gapp.christian@gmail.com>
2018-10-18 16:48:45 +0200

    Add X-AlpineBits-ClientProtocolVersion handling middleware

---
+++ alpinebits-http/impl/src/test/java/it/idm/alpinebits/http/middleware/AlpineBitsClientProtocolMiddlewareIT.java
@@ -0,0 +1,67 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package it.idm.alpinebits.http.middleware;
+
+import it.idm.alpinebits.http.impl.AlpineBitsServlet;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.annotations.Test;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.core.StringContains.containsString;
+
+/**
+ * Integration tests for {@link AlpineBitsClientProtocolMiddleware}.
+ */
+public class AlpineBitsClientProtocolMiddlewareIT extends Arquillian {
+
+    @ArquillianResource
+    private URL base;
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        final WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
+                .addClasses(AlpineBitsServlet.class)
+                .addClasses(AlpineBitsClientProtocolMiddleware.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsWebInfResource("web-alpinebits-client-protocol-middleware-integration-test.xml", "web.xml");
+
+        return war;
+    }
+
+    @Test
+    @RunAsClient
+    public void testHandleContext_AlpineBitsClientProtocolHeaderMissing() {
+        when()
+                .post(this.base + "AlpineBits")
+                .then()
+                .statusCode(HttpServletResponse.SC_BAD_REQUEST)
+                .and()
+                .content(containsString("ERROR:"));
+    }
+
+    @Test
+    @RunAsClient
+    public void testHandleContext_AlpineBitsClientProtocolHeaderDefined() {
+        given()
+                .header(AlpineBitsClientProtocolMiddleware.CLIENT_PROTOCOL_VERSION_HEADER, "2017-10")
+                .when()
+                .post(this.base + "AlpineBits")
+                .then()
+                .statusCode(HttpServletResponse.SC_OK);
+    }
+
+}
