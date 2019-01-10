b0c24236e946f941ebaebce54b653672a3de9e46
gappc <gapp.christian@gmail.com>
2019-01-10 21:00:55 +0100

    Implement AlpineBits FreeRooms example
    The example provides a server implementation for AlpineBits
    FreeRooms with a DB for persistence.
    
    The AlpineBits FreeRooms comes not with a UI, because its
    implementation would be a bit more complex. Currently,
    that effort is not justified.

---
+++ examples/freerooms/src/main/java/it/bz/idm/alpinebits/examples/freerooms/mapper/AvailStatusEntityMapperInstances.java
@@ -0,0 +1,24 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package it.bz.idm.alpinebits.examples.freerooms.mapper;
+
+import org.mapstruct.factory.Mappers;
+
+/**
+ * This class provides mapper instances for
+ * AvailStatus entities.
+ */
+public final class AvailStatusEntityMapperInstances {
+
+    public static final AvailStatusEntityMapper AVAIL_STATUS_ENTITY_MAPPER
+            = Mappers.getMapper(AvailStatusEntityMapper.class);
+
+    private AvailStatusEntityMapperInstances() {
+        // Empty
+    }
+
+}
