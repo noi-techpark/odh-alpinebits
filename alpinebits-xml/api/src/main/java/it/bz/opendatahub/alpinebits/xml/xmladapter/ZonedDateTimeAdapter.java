f9b1a694873ffade15a0b7a967274ef7a7cdf244
gappc <gapp.christian@gmail.com>
2019-10-22 23:01:11 +0200

    Improve ZonedDateTimeAdapter to support datetimes without zone info
    The xsd:dateTime element allows to specify a datetime without zone
    info, for example "2017-09-03T19:47:50" instead of
    "2017-09-03T19:47:50+00:00".
    
    This commit adds support for that kind of datetime values.

--- alpinebits-xml/api/src/main/java/it/bz/opendatahub/alpinebits/xml/xmladapter/ZonedDateTimeAdapter.java
+++ alpinebits-xml/api/src/main/java/it/bz/opendatahub/alpinebits/xml/xmladapter/ZonedDateTimeAdapter.java
@@ -7,8 +7,12 @@
 package it.bz.opendatahub.alpinebits.xml.xmladapter;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.text.ParsePosition;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 
 /**
  * XML adapter to convert xs:dateTime values to {@link ZonedDateTime}.
@@ -16,11 +20,27 @@ import java.time.format.DateTimeFormatter;
 public class ZonedDateTimeAdapter extends XmlAdapter<String, ZonedDateTime> {
     @Override
     public ZonedDateTime unmarshal(String v) {
-        return ZonedDateTime.parse(v, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+
+        if (this.isParseableAsZonedDateTime(v)) {
+            // If the String contains a parsable timezone, it is parsed with that timezone
+            return ZonedDateTime.parse(v, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        } else {
+            // If the String contains no parsable timezone, it is parsed with the system timezone
+            return LocalDateTime.parse(v).atZone(ZoneId.systemDefault());
+        }
     }
 
     @Override
     public String marshal(ZonedDateTime v) {
         return v != null ? v.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME) : null;
+    }
+
+    public boolean isParseableAsZonedDateTime(final CharSequence text) {
+        ParsePosition pos = new ParsePosition(0);
+        TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_ZONED_DATE_TIME.parseUnresolved(text, pos);
+        if (temporalAccessor == null || pos.getErrorIndex() >= 0 || pos.getIndex() < text.length()) {
+            return false;
+        }
+        return true;
     }
 }
