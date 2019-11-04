c2cb5d12676c3f163f60884cbd06e3fef6e4eeb1
gappc <gapp.christian@gmail.com>
2019-11-04 21:17:22 +0100

    Add AlpineBits version checker
    At the moment, the implementation provides a method to check if a given
    string equals a known AlpineBits legacy version.

---
+++ alpinebits-common/api/src/main/java/it/bz/opendatahub/alpinebits/common/AlpineBitsVersionChecker.java
@@ -0,0 +1,40 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package it.bz.opendatahub.alpinebits.common;
+
+import it.bz.opendatahub.alpinebits.common.constants.AlpineBitsVersion;
+
+/**
+ * This class implements facilities to check AlpineBits versions
+ * for different criteria.
+ */
+public class AlpineBitsVersionChecker {
+
+    /**
+     * This method checks if the provided AlpineBits Version is a legacy
+     * version as defined in AlpineBits 2018-10 going on.
+     *
+     * @param alpineBitsVersion The AlpineBits version to check.
+     * @return <code>true</code> if the provided version is a legacy
+     * version, <code>false</code> otherwise.
+     */
+    public static boolean isLegacyVersion(String alpineBitsVersion) {
+        return AlpineBitsVersion.V_2017_10.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2015_07B.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2015_07.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2014_04.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2013_04.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2012_05B.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2012_05.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2011_11.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2011_10.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2011_09.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2010_10.equals(alpineBitsVersion)
+                || AlpineBitsVersion.V_2010_08.equals(alpineBitsVersion);
+    }
+
+}
